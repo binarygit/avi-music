a929fe028207dbea1426d5fa6f3d670d18b0cb79
binarygit <inscription4@outlook.com>
2023-11-30 21:54:25 +0545

    Write tests for SongComponent

--- test/components/song_component_test.rb
+++ test/components/song_component_test.rb
@@ -3,10 +3,14 @@
 require "test_helper"
 
 class SongComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(SongComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_song_component
+    @songs = Song.all
+    render_inline(SongComponent.with_collection(@songs))
+
+    @songs.each do |song|
+      selector = "a[data-href='songs/#{song.title}'][data-id='#{song.id}'][data-action='click->music#play']"
+      assert_selector selector
+      assert_selector "a[href='##{song.title}']"
+    end
   end
 end
